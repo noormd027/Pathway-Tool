eb32f8719fc4505ea2b844edaed3b141d5666de3
mjmanning25 <mjmanning25@users.noreply.github.com>
2019-09-18 11:48:41 +1200

    Created the admin login page
    Added the HTML for the admin login page
    
    Needs:
    PHP and CSS

---
+++ files/adminLogin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Admin Login</title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="post">
+      <input type="text" name="uname" value="" placeholder="username">
+      <input type="password" name="pword" value="" placeholder="password">
+      <input type="submit" name="" value="submit">
+    </form>
+
+  </body>
+</html>
