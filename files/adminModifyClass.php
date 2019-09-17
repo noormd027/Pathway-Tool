67108a020424007859896ce2d96b7e0a58b76b9c
mjmanning25 <mjmanning25@users.noreply.github.com>
2019-09-18 11:55:02 +1200

    Created the admin modify classes page
    Added the base HTML for the admin modify classes page
    
    Needed:
    PHP
    CSS
    Implement new input fields based on database design

---
+++ files/adminModifyClass.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Admin modify class</title>
+</head>
+<body>
+  <form class="" action="index.html" method="post">
+    <input type="text" name="" value="" placeholder="class name">
+    <br>
+    <input type="text" name="" value="" placeholder="class details">
+    <br>
+    <input type="text" name="" value="" placeholder="more stuff needed">
+    <br>
+    <input type="text" name="" value="" placeholder="below">
+    <input type="submit" name="" value="submit">
+  </form>
+</body>
+</html>
