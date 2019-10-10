1892f3a4d0693da4a0b5442889fea23c5988a6e0
mjmanning25 <mjmanning25@gmail.com>
2019-10-10 18:46:56 +1300

    Changed file types to .php from .html so that I can begin work on the PHP code

--- files/adminModifyClass.php
+++ files/adminModifyClass.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Admin modify class</title>
-  <link rel="stylesheet" href="/css/master.css">
+  <link rel="stylesheet" href="./style.css">
 </head>
 <body>
   <h1>Modify Class</h1>
