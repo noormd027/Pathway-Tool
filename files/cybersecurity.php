a359c51c0a93a9cd905241bcb028bf652fe2b878
mjmanning25 <mjmanning25@gmail.com>
2019-10-10 19:22:29 +1300

    Major code overhaul, added comments and notes.
    added:
    - connection to database
    - querry for database
    - while loop to display all degrees (based on the database diagram that 
    is currently availiable)
    - created a dynamic list of links that will display the correct pathways 
    attached to a course
    - added heavy comments to who work
    - reformatted the page slightly
    - fixed whitespace in document

--- files/cybersecurity.php
+++ files/cybersecurity.php
@@ -4,63 +4,66 @@ include "./xcommon.php";
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Classes in Cybersecurity</title>
-    <link rel="stylesheet" href="./style.css">
-  </head>
-  <body>
-    <nav>
-      <ul>
-        <li><a href="https://www.unitec.ac.nz/" target="_blank">Unitec</a></li>
-        <li><a href="index.html" target="_blank">Pathway Planner</a></li>
-      </ul>
-    </nav>
-    <h1>Cybersecurity Pathway</h1>
-    <div class="classes">
-      <ul>
-        <h3>First Year</h3>
-        <li><a href="#">ISCG5400 Hardware Fundaments</a></li>
-        <li><a href="#">ISCG5401 Operating System Funcdamentals</a></li>
-        <li><a href="#">ISCG5420 Programming Fundamentals</a></li>
-        <li><a href="#">ISCG5430 Prof Skills IT Practitioners</a></li>
-      </ul>
-      <ul>
-        <h3>Second Year</h3>
-        <li><a href="#">ISCG6400 Hardware Technology</a></li>
-        <li><a href="#">ISCG6401 Data Communications & Networks</a></li>
-        <li><a href="#">ISCG6413 Testing & Quality Assurance</a></li>
-        <li><a href="#">ISCG6411 Project Planning & Control</a></li>
-      </ul>
-      <ul>
-        <h3>Third Year</h3>
-        <li><a href="#">ISCG7430 Project</a></li>
-        <li><a href="#">ISCG7431 Capstone Project</a></li>
-        <li><a href="#">ISCG7407 Advanced Cybersecurity</a></li>
-        <li><a href="#">ISCG7404 Computer Forensics Investigation</a></li>
-      </ul>
-    </div>
-<div class="selectClasses">
-  <form class="selectClasses" action="selectClasses.php" method="post">
-    <h3>Select Your Classes</h3>
-    First Year <br>
-    <input type="checkbox" name="ISCG5400 Hardware Fundaments" value="ISCG5400" tabindex="1"/>ISCG5400 Hardware Fundaments </br>
-    <input type="checkbox" name="ISCG5401 Operating System Funcdamentals" value="ISCG5401" tabindex="2"/>ISCG5401 Operating System Funcdamentals </br>
-    <input type="checkbox" name="ISCG5420 Programming Fundamentals" value="ISCG5420" tabindex="3"/>ISCG5420 Programming Fundamentals </br>
-    <input type="checkbox" name="ISCG5430 Prof Skills IT Practitioners" value="ISCG5430" tabindex="4"/>ISCG5430 Prof Skills IT Practitioners </br>
-    Second Year <br>
-    <input type="checkbox" name="ISCG6400 Hardware Technology" value="ISCG6400" tabindex="5"/>ISCG6400 Hardware Technology </br>
-    <input type="checkbox" name="ISCG6401 Data Communications & Networks" value="ISCG6401" tabindex="6"/>ISCG6401 Data Communications & Networks</br>
-    <input type="checkbox" name="ISCG6413 Testing & Quality Assurance" value="ISCG6413" tabindex="7"/>ISCG6413 Testing & Quality Assurance </br>
-    <input type="checkbox" name="ISCG6411 Project Planning & Control" value="ISCG6411" tabindex="8"/>ISCG6411 Project Planning & Control </br>
-    Third Year <br>
-    <input type="checkbox" name="ISCG7430 Project" value="ISCG7430" tabindex="9"/>ISCG7430 Project </br>
-    <input type="checkbox" name="ISCG7431 Capstone Project" value="ISCG7431" tabindex="10"/>ISCG7431 Capstone Project</br>
-    <input type="checkbox" name="ISCG7407 Advanced Cybersecurity" value="ISCG7407" tabindex="11"/>ISCG7407 Advanced Cybersecurity </br>
-    <input type="checkbox" name="ISCG7404 Computer Forensics Investigation" value="ISCG7404" tabindex="12"/>ISCG7404 Computer Forensics Investigation </br>
-    <input type="submit" name="Print" value="Print">
+<head>
+  <meta charset="utf-8">
+  <title>Classes in Cybersecurity</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+  <nav>
+    <ul>
+      <li><a href="https://www.unitec.ac.nz/" target="_blank">Unitec</a></li>
+      <li><a href="index.html" target="_blank">Pathway Planner</a></li>
+    </ul>
+  </nav>
+  <h1>Cybersecurity Pathway</h1>
+  <div class="classes">
+    <ul>
+      <?php // This will be its own search querry for the database ?>
+      <h3>First Year</h3>
+      <li><a href="#">ISCG5400 Hardware Fundaments</a></li>
+      <li><a href="#">ISCG5401 Operating System Funcdamentals</a></li>
+      <li><a href="#">ISCG5420 Programming Fundamentals</a></li>
+      <li><a href="#">ISCG5430 Prof Skills IT Practitioners</a></li>
+    </ul>
+    <ul>
+      <?php // This will be its own search querry for the database ?>
+      <h3>Second Year</h3>
+      <li><a href="#">ISCG6400 Hardware Technology</a></li>
+      <li><a href="#">ISCG6401 Data Communications & Networks</a></li>
+      <li><a href="#">ISCG6413 Testing & Quality Assurance</a></li>
+      <li><a href="#">ISCG6411 Project Planning & Control</a></li>
+    </ul>
+    <ul>
+      <?php // This will be its own search querry for the database ?>
+      <h3>Third Year</h3>
+      <li><a href="#">ISCG7430 Project</a></li>
+      <li><a href="#">ISCG7431 Capstone Project</a></li>
+      <li><a href="#">ISCG7407 Advanced Cybersecurity</a></li>
+      <li><a href="#">ISCG7404 Computer Forensics Investigation</a></li>
+    </ul>
+  </div>
+  <div class="selectClasses">
+    <form class="selectClasses" action="selectClasses.php" method="post">
+      <h3>Select Your Classes</h3>
+      First Year <br>
+      <input type="checkbox" name="ISCG5400 Hardware Fundaments" value="ISCG5400" tabindex="1"/>ISCG5400 Hardware Fundaments </br>
+      <input type="checkbox" name="ISCG5401 Operating System Funcdamentals" value="ISCG5401" tabindex="2"/>ISCG5401 Operating System Funcdamentals </br>
+      <input type="checkbox" name="ISCG5420 Programming Fundamentals" value="ISCG5420" tabindex="3"/>ISCG5420 Programming Fundamentals </br>
+      <input type="checkbox" name="ISCG5430 Prof Skills IT Practitioners" value="ISCG5430" tabindex="4"/>ISCG5430 Prof Skills IT Practitioners </br>
+      Second Year <br>
+      <input type="checkbox" name="ISCG6400 Hardware Technology" value="ISCG6400" tabindex="5"/>ISCG6400 Hardware Technology </br>
+      <input type="checkbox" name="ISCG6401 Data Communications & Networks" value="ISCG6401" tabindex="6"/>ISCG6401 Data Communications & Networks</br>
+      <input type="checkbox" name="ISCG6413 Testing & Quality Assurance" value="ISCG6413" tabindex="7"/>ISCG6413 Testing & Quality Assurance </br>
+      <input type="checkbox" name="ISCG6411 Project Planning & Control" value="ISCG6411" tabindex="8"/>ISCG6411 Project Planning & Control </br>
+      Third Year <br>
+      <input type="checkbox" name="ISCG7430 Project" value="ISCG7430" tabindex="9"/>ISCG7430 Project </br>
+      <input type="checkbox" name="ISCG7431 Capstone Project" value="ISCG7431" tabindex="10"/>ISCG7431 Capstone Project</br>
+      <input type="checkbox" name="ISCG7407 Advanced Cybersecurity" value="ISCG7407" tabindex="11"/>ISCG7407 Advanced Cybersecurity </br>
+      <input type="checkbox" name="ISCG7404 Computer Forensics Investigation" value="ISCG7404" tabindex="12"/>ISCG7404 Computer Forensics Investigation </br>
+      <input type="submit" name="Print" value="Print">
 
-  </form>
+    </form>
 
   </body>
-</html>
+  </html>
--- files/index.php
+++ files/index.php
@@ -1,58 +1,91 @@
 <?php
 include "./xcommon.php";
+
+// Note to the devs of this page!
+// commented code is relevant for later on, but not now
+//
+
+//if (empty($_REQUEST['i'])) $_REQUEST['i'] = '1';
+//switch($_REQUEST['i'])
+//{
+//  case '1':
+//  ListDegrees(0);
+//  break;
+//  case '2':
+//  tempThing();
+//  break;
+//  default:
+//  ListDegrees(0);
+//}
+
+$ProgrammeQuerry = "SELECT * FROM Programme order by ProgrammeName ASC;";
+//$PathwayQuerry = "SELECT * FROM Pathway order by PathwayName ASC;";
+$ProgrammeResults = mysqli_query($conn, $ProgrammeQuerry) or die(mysqli_error($conn));
+//$PathwayResults = mysqli_query($conn, $PathwayQuerry) or die(mysqli_error($conn));
+
 ?>
+
 <!DOCTYPE html>
-
 <html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Pathway Planner</title>
+  <link rel="stylesheet" href="./style.sss">
+</head>
 
-  <head>
-    <meta charset="utf-8">
-    <title>Pathway Planner</title>
-    <link rel="stylesheet" href="./style.sss">
+<body>
+  <nav>
+    <ul>
+      <li><a href="https://www.unitec.ac.nz/" target="_blank">Unitec</a></li>
+      <li><a href="#">Information</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
 
-  </head>
+  <h1>Pathway Planner</h1>
+  <h2>Select Your Study Options</h2>
+  <ul class="courseSelections">
+    <?php
+    // this code is sooo bad, this needs fixing, currently parsing the id to
+    // another page to do the processing and show the list of attached pathways
+    // but this can be re-written so only this page is needed by using a switch
+    // however this works for what we have currently developed.
+    while ($programmes = mysqli_fetch_array($ProgrammeResults))
+    {
+      $ID = $programmes['ProgrammeID']
+      $Name = $programmes['ProgrammeName'];
 
-  <body>
+      // this is the redirect code, looks messy but you should get an output like:
+      // mikex.co.nz/testing/pathways.php?id=xxx
+      // this should work, once there is a pathways.php page setup
+      ?>
+      <li><?php echo $ID." - ".$Name; ?> - <a href="pathways.php?id=<?php echo $ID; ?>">Temp Link</a> </li>
+      <?php
+    }
+    ?>
 
-    <nav>
+    <!--The code below is fine, however having static lists is bad, and we will re-write this later-->
 
-      <ul>
-        <li><a href="https://www.unitec.ac.nz/" target="_blank">Unitec</a></li>
-        <li><a href="#">Information</a></li>
-        <li><a href="#">Contact</a></li>
+    <ul class="Bachelors">
+      <li><a href="#">Bachelor of Architectural Techlogy</a></li>
+      <li><a href="#">Bachelor of Computer Systems</a>
 
-      </ul>
-
-    </nav>
-    <h1>Pathway Planner</h1>
-
-        <h2>Select Your Study Options</h2>
-        <ul class="courseSelections">
-          <li><a href="#">Certificates/Diplomas</a></li>
-          <li><a href="#">Bachelors</a>
-            <ul class="Bachelors">
-              <li><a href="#">Bachelor of Architectural Techlogy</a></li>
-              <li><a href="#">Bachelor of Computer Systems</a>
-                <ul>
-                  <li><a href="#">Computer Networks and Cloud Compting</a></li>
-                  <li><a href="#">Software Engineering</a></li>
-                  <li><a href="#">Games Development</a></li>
-                  <li><a href="#">Business Intellingence</a></li>
-                  <li><a href="cybersecurity.html" target="_blank">Cybersecurity</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Bachelor of Nursing</a></li>
-              <li><a href="#">Bachelor of Osteopath</a></li>
-            </ul>
-
-          </li>
-          <li><a href="#">Graduate Diplomas</a></li>
-          <li><a href="#">Postgraduates</a></li>
+        <ul>
+          <li><a href="#">Computer Networks and Cloud Compting</a></li>
+          <li><a href="#">Software Engineering</a></li>
+          <li><a href="#">Games Development</a></li>
+          <li><a href="#">Business Intellingence</a></li>
+          <li><a href="cybersecurity.html" target="_blank">Cybersecurity</a></li>
         </ul>
+      </li>
 
+      <li><a href="#">Bachelor of Nursing</a></li>
+      <li><a href="#">Bachelor of Osteopath</a></li>
+    </ul>
+  </li>
 
-
-
-
-  </body>
+  <li><a href="#">Graduate Diplomas</a></li>
+  <li><a href="#">Postgraduates</a></li>
+</ul>
+</body>
 </html>
