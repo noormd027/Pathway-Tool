9217580331742051ec7f9fd6e181487399a8d173
mjmanning25 <mjmanning25@gmail.com>
2019-10-18 12:05:26 +1300

    Major page overhaul, bulk code for displaying database information has been written and needs to be tested

--- files/pathways.php
+++ files/pathways.php
@@ -2,7 +2,7 @@
 include "./xcommon.php";
 
 // get the id from the previous page to use here
-$id = $_REQUEST['id']
+$id = $_REQUEST['id'];
 
 if (empty($_REQUEST['i'])) $_REQUEST['i'] = '1';
 switch($_REQUEST['i'])
@@ -23,7 +23,7 @@ switch($_REQUEST['i'])
 
 ?>
 
-  <p>Sort by: | <a href='./pathways.php?i=1'>Class ID</a> | <a href='./pathways.php?i=2'>Class Name</a> | <a href='./pathways.php?i=3'>Year</a></p>
+  <p>Sort by: | <a href='./pathways.php?i=1&id=<?php echo $id ?>'>Class ID</a> | <a href='./pathways.php?i=2&id=<?php echo $id ?>'>Class Name</a> | <a href='./pathways.php?i=3&id=<?php echo $id ?>'>Year</a></p>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -39,6 +39,44 @@ switch($_REQUEST['i'])
       <li><a href="index.html" target="_blank">Pathway Planner</a></li>
     </ul>
   </nav>
+
+  <?php
+
+  // use the id that is parsed to this page to determine the title of the programme
+  // then set the variable PTitle to be the right rewsult, making this page modular
+  $gettitle = "select * from Programme where ProgrammeID=$id";
+  $titleresult = mysqli_query($conn, $gettitle) or die(mysqli_error($conn));
+
+  // bad logic but currently only 1 needs to be selected
+  while ($mytitle = mysqli_fetch_array($titleresult))
+  {
+    $PTitle = $titleresult['ProgrammeName'];
+
+    // start formatting the page and print the title
+    echo "<h1>".$PTitle."</h1>";
+  }
+
+  while ($PathwayResults = mysqli_fetch_array($results))
+  {
+    $PWID = $PathwayResults['PathwayID'];
+    $PWName = $PathwayResults['PathwayName'];
+
+    // segment each pathway into  its own box
+    echo "<div>";
+
+    // display each pathway
+    echo "<h2>".$PWName."</h2>";
+    echo "<h3>".$PWID."</h3>";
+
+    //create a link to the next page, parsing the pathwayID
+    echo "<a href='./courses.php?id=".$PWID."'>Show Courses in Pathway</a>";
+
+    // close the block
+    echo "</div>";
+  }
+
+  ?>
+
   <h1>Cybersecurity Pathway</h1>
   <div class="classes">
     <ul>
